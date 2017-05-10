87801fa0264c0d2c27cd75023a56de8cb4f27a80
merk <m_n_m_n_m@mail.ru>
2017-05-11 01:54:29 +0300

    Отказ от всех кривых  адаптаций

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  Rails.application.routes.default_url_options[:host] = 'http://merkmerk.herokuapp.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -77,4 +78,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-Rails.application.routes.default_url_options[:host] = 'http://merkmerk.herokuapp.com'
